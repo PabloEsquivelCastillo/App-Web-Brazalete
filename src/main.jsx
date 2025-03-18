64d7cf207f48f059d0951ef61a0ee44ca61df6d7
PabloEsquivelCastillo <20233tn066@utez.edu.mx>
2025-03-18 16:09:31 -0600

    Configuracion main y app

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importar Bootstrap CSS
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <AuthProvider>
+    <App/>
+  </AuthProvider>
 )
