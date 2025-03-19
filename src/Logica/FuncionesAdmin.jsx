d54c5b1ad5d28ca178b02c9e7f83b42cd8af3a41
PabloEsquivelCastillo <20233tn066@utez.edu.mx>
2025-03-18 23:04:24 -0600

    Service con funciones de endpoints

--- src/Logica/FuncionesAdmin.jsx
+++ src/Logica/FuncionesAdmin.jsx
@@ -1,1 +1,29 @@
 //Aquí para manejar la lógica del admin
+
+import axiosInstance from '../api/axiosConfig'; // Importa la instancia configurada de Axios
+
+// Obtener la lista de cuidadores
+export const getCuidadores = async () => {
+    try {
+        const response = await axiosInstance.get('/api/users/listKeepers'); // Usa la instancia configurada
+        return response.data; // Devuelve los datos de la respuesta
+    } catch (error) {
+        console.error('Error fetching keepers:', error);
+        throw error; 
+    }
+};
+
+
+// Desactivar un cuidador
+export const deactivateCuidador = async (id) => {
+    try {
+        const response = await axiosInstance.put(`/api/users/deactivate/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error deactivating keeper:', error);
+        throw error;
+    }
+};
+
+
+//Actualizar Cuidador
