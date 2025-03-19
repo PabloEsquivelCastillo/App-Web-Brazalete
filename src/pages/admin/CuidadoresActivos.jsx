de6bc2d2dea333839524065d32897f92d033ab01
PabloEsquivelCastillo <20233tn066@utez.edu.mx>
2025-03-18 23:03:52 -0600

    Desactivar cuidadores

--- src/pages/admin/CuidadoresActivos.jsx
+++ src/pages/admin/CuidadoresActivos.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { getCuidadores } from "../../components/CuidadorService";
+import { getCuidadores, deactivateCuidador } from "../../Logica/FuncionesAdmin";
 import '../../css/CuidadoresActivos.css'
 import Navbar from "../../components/Navbar";
 import LateralAdmin from "../../components/LateralAdmin";
+import { MdDeleteOutline } from "react-icons/md";
+import { FaRegEdit } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 
 const CuidadoresActivos = () => {
@@ -17,7 +20,7 @@ const CuidadoresActivos = () => {
                 const data = await getCuidadores();// Llama a la función getCuidadores
                 setCuidadores(data); // Guarda los datos en el estado
             } catch (err) {
-                setError(err); // Maneja el error
+                setError(err);
             } finally {
                 setLoading(false); // Finaliza la carga
             }
@@ -25,6 +28,21 @@ const CuidadoresActivos = () => {
 
         cargarCuidadores();
     }, []);
+
+    // Función para manejar la desactivación de un cuidador
+    const handleDeactivate = async (id) => {
+        if (window.confirm("¿Estás seguro de que deseas desactivar este cuidador?")) {
+            try {
+                await deactivateCuidador(id);
+                const updatedCuidadores = cuidadores.filter(cuidador => cuidador._id !== id);
+                setCuidadores(updatedCuidadores);
+                toast.success("Cuidador desactivado correctamente");
+            } catch (error) {
+                console.error("Error al desactivar el cuidador:", error);
+                toast.error("Error al desactivar el cuidador");
+            }
+        }
+    };
 
     if (loading) return <p>Cargando cuidadores...</p>; // Muestra un mensaje de carga
     if (error) return <p>Error al cargar los cuidadores: {error.message}</p>; // Muestra un mensaje de error
@@ -35,31 +53,40 @@ const CuidadoresActivos = () => {
 
     return (
         <>
-                <Navbar/>
-                <LateralAdmin/>
-                <div className="container">
-            
-            <table className="table table-custom">
-                <thead>
-                    <tr>
-                        <th scope="col" className="col-name">Nombre</th>
-                        <th scope="col">Correo electrónico</th>
-                        <th className= "border-two" scope="col">Teléfono</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {cuidadores.map((cuidador) => (
-                        <tr key={cuidador._id}>
-                            <td>{cuidador.name}</td>
-                            <td>{cuidador.email}</td>
-                            <td>{cuidador.phone}</td>
+            <div className="container">
+                <table className="table table-custom">
+                    <thead>
+                        <tr>
+                            <th className="boreder-one" scope="col" >id</th>
+                            <th scope="col" >Nombre</th>
+                            <th scope="col">Correo electrónico</th>
+                            <th scope="col">Teléfono</th>
+                            <th className="border-two" scope="col">Acciones</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        {cuidadores.map((cuidador) => (
+                            <tr key={cuidador._id}>
+                                <td>{cuidador._id}</td>
+                                <td>{cuidador.name}</td>
+                                <td>{cuidador.email}</td>
+                                <td>{cuidador.phone}</td>
+                                <td>
+                                    <button className="btn btn-update" >
+                                        <FaRegEdit />
+                                    </button>
+                                    <button className="btn btn-delete"  onClick={() => handleDeactivate(cuidador._id)}>
+                                        <MdDeleteOutline />
+                                    </button>
+
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </>
-        
+
     );
 };
 
