c87f75d937ff4bd9c770070fa280827f6433c782
PabloEsquivelCastillo <20233tn066@utez.edu.mx>
2025-03-20 00:44:33 -0600

    Rutas añadidas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,10 +3,11 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 
 import Login from '../pages/Login';
 import { useAuth } from '../context/AuthContext';
-import MenuCuidador from '../pages/admin/CuidadoresActivos';
 import CuidadoresActivos from '../pages/admin/CuidadoresActivos';
 import Solicitudes from '../pages/admin/SolicitudesPendientes';
 import EditarCuidador from '../pages/admin/EditarCuidador';
+import MenuCuidador from '../pages/cuidador/MenuCuidador';
+import SolicitudesPendientes from '../pages/admin/SolicitudesPendientes';
 
 function AppRoutes() {
   const { user } = useAuth(); // Obtener el usuario del contexto de autenticación
@@ -15,46 +16,25 @@ function AppRoutes() {
     <Router>
       <Routes>
         {/* Rutas públicas */}
-
         <Route path="/" element={<Login />} />
 
         {/* Rutas protegidas para admin */}
-        <Route
-          path="/admin/dashboard"
-          element={
-            user?.rol === 'admin' ? ( // Verificar si el usuario es admin
-              <CuidadoresActivos/>
-            ) : (
-              <Navigate to="/login" /> // Redirigir al login si no es admin
-            )
-          }
-        />
-        <Route
-        path='editar/:id'
-        element={<EditarCuidador/>}/>
-
-<Route
-          path="/admin/solicitudes"
-          element={
-            user?.rol === 'admin' ? ( // Verificar si el usuario es admin
-              <Solicitudes/>
-            ) : (
-              <Navigate to="/login" /> // Redirigir al login si no es admin
-            )
-          }
-        />
+        {user?.rol === 'admin' && (
+          <>
+            <Route path="/admin/dashboard" element={<SolicitudesPendientes />} />
+            <Route path="/admin/cuidadoresActivos" element={<CuidadoresActivos />} />
+            <Route path="/admin/solicitudes" element={<Solicitudes />} />
+            <Route path="/admin/editar/:id" element={<EditarCuidador />} />
+          </>
+        )}
 
         {/* Rutas protegidas para cuidador */}
-        <Route
-          path="/cuidador/dashboard"
-          element={
-            user?.rol === 'cuidador' ? ( // Verificar si el usuario es cuidador
-              <MenuCuidador />
-            ) : (
-              <Navigate to="/login" /> // Redirigir al login si no es cuidador
-            )
-          }
-        />
+        {user?.rol === 'cuidador' && (
+          <Route path="/cuidador/dashboard" element={<MenuCuidador />} />
+        )}
+
+        {/* Redirigir si la ruta no existe o el usuario no tiene permisos */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
